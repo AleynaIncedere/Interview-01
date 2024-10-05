9e5b9b15df388db84a0b3e7a63134a014c81d4a8
AleynIncedere <redlack17@gmail.com>
2024-10-05 18:41:46 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+function App() {
+  const [nestedObjected, setNestedObject] = useState({
+    taxi: 'Ücret karşılığında yolcu taşımak için lisanslı bir araç',
+    food: {
+      sushi: 'Deniz ürünleri ve sebzelerle birlikte sunulan geleneksel bir Japon yemeği',
+      apple: {
+        Honeycrisp:
+          "MAES Bahçe Araştırma Merkezi'nde geliştirilen bir elma çeşidi",
+        Fuji: "Tohoku Araştırma İstasyonu'nda yetiştiriciler tarafından geliştirilen bir elma çeşidi",
+      },
+    },
+  });
+
+  return (
+    <div style={{ margin: 'auto', width: '70%', paddingTop: 40 }}>
+      <DisplayNested nestedObjected={nestedObjected} />
+    </div>
+  );
+}
+
+const DisplayNested = ({ nestedObjected }) => {
+  return (
+    <ul>
+      {Object.entries(nestedObjected).map(([key, value]) => {
+        return (
+          <li key={key}>
+            <strong>{key}</strong>: {typeof value === 'string' ? value : <DisplayNested nestedObjected={value} />}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default App;
